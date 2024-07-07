a05f4d3482eaa80fb9e1ca5bfde5dbf7d9daffc3
Max Perona <perona.max01@gmail.com>
2024-07-07 16:07:50 +0200

    particles options

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,87 +1,18 @@
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+import { particlesOptions } from "./particlesOptions";
 
 export const Cover = () => {
+  const particlesInit = async (main: any) => {
+    await loadFull(main);
+  };
+
   return (
     <Particles
       id="tsparticles"
-      options={{
-        background: {
-          color: {
-            value: "#f9f9f9",
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#000",
-          },
-          links: {
-            color: "#000",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
+      init={particlesInit}
+      options={particlesOptions}
     />
   );
 };
